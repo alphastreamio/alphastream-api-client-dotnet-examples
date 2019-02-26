85041ef8fc2b05df8569b12a11d7215f9670d251
Darren Benn <darrenbenn@gmail.com>
2019-02-26 16:26:09 +0000

    Update to demonstrate deleting a watchlist via the delete members API

---
+++ ASP.NET/MvcExample/Models/WatchlistViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AlphaStream.ApiClient.Watchlists.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcExample.Models
+{
+    public class WatchlistViewModel : Watchlist
+    {
+        [Display(Name = "New Watchlist Id")]
+        public string NewWatchlistId { get; set; }
+    }
+}
